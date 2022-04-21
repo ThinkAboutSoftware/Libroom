eb27d9f3d6cf0d0edd21b3b1b4cea8809204c002
kyungup <dlrkql0007@gmail.com>
2022-04-21 19:31:49 +0900

    CreatorSceneChange

--- Assets/02.Scripts/URL.cs
+++ Assets/02.Scripts/URL.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 
 public class URL : MonoBehaviour
 {
+    //이경업 링크
     public void KyungUpURL()
     {
         Application.OpenURL("https://www.instagram.com/l.k.upp/");
     }
 
+    public void KyungUpGithub()
+    {
+        Application.OpenURL("https://github.com/lku0716");
+    }
+
+    //김태우 링크
     public void TaeWooURL()
     {
         Application.OpenURL("https://www.instagram.com/xod5195/");
     }
+    public void TaeWooGithub()
+    {
+        Application.OpenURL("https://github.com/M2MariMine");
+    }
 
+    //김서영 링크
     public void SeoYoung()
     {
         Application.OpenURL("https://www.instagram.com/ii__syeong/");
     }
+    public void SeoYoungGithub()
+    {
+        Application.OpenURL("https://github.com/seo0young");
+    }
 
+    //조유빈 링크
     public void YouBean()
     {
         Application.OpenURL("https://www.instagram.com/u.__.kong/");
+    }
+
+    public void YouBeanGithub()
+    {
+        Application.OpenURL("https://github.com/dbqls0814");
     }
 }
