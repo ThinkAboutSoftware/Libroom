2427d2e04a2c51d5a01d17b5cdd9b85d44c866e8
kyungup <dlrkql0007@gmail.com>
2022-04-29 22:54:18 +0900

    room prefab

---
+++ Assets/02.Scripts/ObjectColorChange.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectColorChange : MonoBehaviour
+{
+    public GameObject[] bodyObject;
+    public Color32[] colors;
+    Material[] deskMats;
+
+    void Start()
+    {
+        deskMats = new Material[bodyObject.Length];
+        for (int i = 0; i < deskMats.Length; i++)
+        {
+            deskMats[i] = bodyObject[i].GetComponent<MeshRenderer>().material;
+        }
+        colors[0] = deskMats[0].color;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeColor(int num)
+    {
+        for (int i = 0; i < deskMats.Length; i++)
+        {
+            deskMats[i].color = colors[num];
+        }
+    }
+}
